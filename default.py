36614af9a56528d1db6617c21e44a9518b523576
bharathivasan <bharathivasan346@gmail.com>
2021-09-28 09:52:19 -0700

    this default agruments and variable length arguments

---
+++ default.py
@@ -0,0 +1,23 @@
+# default arguments
+class ABC:
+    def fun1(self, n1 = 0, n2 = 0, n3 = 0):
+        print((n1 * n2) * n3)
+
+num = ABC()
+num.fun1()
+num.fun1(2, 20, 3)
+
+# variable length argument
+
+class abc:
+    def AddionofNNumbers(self, *no):
+        total = 0
+        for num in no:
+            total = total + num
+        print(total)    
+
+abc1 = abc()
+
+abc1.AddionofNNumbers()
+abc1.AddionofNNumbers(10)
+abc1.AddionofNNumbers(10,20,30,40)
